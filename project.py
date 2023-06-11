0e1c8b197ea4e7e9228417a8baf483f41b29da63
Miguel Bustamante <miguel10bustamante21@gmail.com>
2023-06-11 12:40:51 -0600

    Add build with pylint v8

--- project.py
+++ project.py
@@ -1,7 +1,19 @@
-# The purpose of this python program is to test a workflow in gitHub
+"""
+This module provides functionality for gitHub workflows.
+
+This is only a test program to simulate a deployment of a python app.
+
+Author: Miguel Bustamante
+Date: 11/06/2023
+"""
+
 
 def say_hi():
-    # Function that returns a string
+    """
+    Function that returns a string
+
+    This is only for test purposes
+    """
     return 'Hello master'
 
 
