30e63a8c4c6bf37e4cff89b4a3d136514f011cf5
Miguel Bustamante <miguel10bustamante21@gmail.com>
2023-06-12 14:08:24 -0600

    Add port 3000 deployment

--- project.py
+++ project.py
@@ -11,9 +11,15 @@ Date: 11/06/2023
 from flask import Flask
 
 app = Flask(__name__)
+port = 3000
 
 
 @app.route("/")
 def hello_world():
     """Return 'Hello World' as a string."""
     return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    """Run the app in the port 3000 """
+    app.run(port=port)
